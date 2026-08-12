462ae4c3ecbf7f0121e916653b6627d013c1fb9f
zaid963f <mzfotaena@gmail.com>
2026-08-12 22:17:03 +0300

    for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export",
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
